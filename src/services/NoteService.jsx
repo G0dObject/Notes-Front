0405866bcbcd6926c92c6e3248475b13fb6ec828
G0dObject <vladimirgjob@gmail.com>
2023-01-09 14:51:26 +0300

    remove slash before endpoint

--- src/services/NoteService.jsx
+++ src/services/NoteService.jsx
@@ -2,12 +2,12 @@ import $api from "../http";
 
 export default class NoteService {
 	static async get() {
-		return await $api.get("/notes");
+		return await $api.get("notes");
 	}
 	static async delete(noteid) {
-		return await $api.delete(`/notes`, { data: { noteid } });
+		return await $api.delete(`notes`, { data: { noteid } });
 	}
 	static async post(title, text) {
-		return await $api.post(`/notes`, { title, text });
+		return await $api.post(`notes`, { title, text });
 	}
 }
